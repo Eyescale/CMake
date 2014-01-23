2a913e4a072f11563fdc6e2fb85a6915114c29b0
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-01-23 17:50:36 +0100

    Increase cppcheck checking level

--- CppcheckTargets.cmake
+++ CppcheckTargets.cmake
@@ -42,7 +42,8 @@ endif()
 
 function(add_cppcheck_sources _targetname)
 	if(CPPCHECK_FOUND)
-		set(_cppcheck_args --force)
+		set(_cppcheck_args --force -I ${CMAKE_SOURCE_DIR}
+                  --error-exitcode=2 --suppress=unusedFunction)
 		set(_input ${ARGN})
 		list(FIND _input UNUSED_FUNCTIONS _unused_func)
 		if("${_unused_func}" GREATER "-1")
@@ -138,7 +139,8 @@ function(add_cppcheck _name)
 			"add_cppcheck given a target name that does not exist: '${_name}' !")
 	endif()
 	if(CPPCHECK_FOUND)
-		set(_cppcheck_args --force)
+		set(_cppcheck_args --force -I ${CMAKE_SOURCE_DIR}
+                  --error-exitcode=2 --suppress=unusedFunction)
 
 		list(FIND ARGN UNUSED_FUNCTIONS _unused_func)
 		if("${_unused_func}" GREATER "-1")
