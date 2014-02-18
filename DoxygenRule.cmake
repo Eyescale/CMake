655eaa823dde73e5da252546fd73206ef447b352
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-02-18 08:45:43 +0100

    Also generate Doxyfile

--- DoxygenRule.cmake
+++ DoxygenRule.cmake
@@ -3,6 +3,14 @@
 # Configures a Doxyfile and provides doxygen and doxygit targets. Relies on
 # TargetHooks installed by Common and must be included after all targets!
 #
+# Input Variables
+# * DOXYGEN_EXTRA_INPUT appended to INPUT in Doxyfile
+#
+# IO Variables (set if not set as input)
+# * GIT_DOCUMENTATION_REPO or GIT_ORIGIN_org is used
+# * DOXYGEN_CONFIG_FILE or one is auto-configured
+# * COMMON_ORGANIZATION_NAME (from GithubOrganization)
+#
 # * doxygen runs doxygen after compiling and installing the project
 # * doxygit runs doxygen and installs the documentation in
 #   CMAKE_SOURCE_DIR/../GIT_DOCUMENTATION_REPO or
@@ -13,15 +21,31 @@ if(NOT DOXYGEN_FOUND)
   return()
 endif()
 
-if(NOT DOXYGEN_CONFIG_FILE)
-  # Assuming there exists a Doxyfile and that needs configuring
-  configure_file(doc/Doxyfile ${CMAKE_BINARY_DIR}/doc/Doxyfile @ONLY)
-  set(DOXYGEN_CONFIG_FILE ${CMAKE_BINARY_DIR}/doc/Doxyfile)
-endif()
-
 get_property(INSTALL_DEPENDS GLOBAL PROPERTY ALL_DEP_TARGETS)
 if(NOT INSTALL_DEPENDS)
   message(FATAL_ERROR "No targets in CMake project, Common.cmake not used?")
+endif()
+
+if(NOT GIT_DOCUMENTATION_REPO)
+  include(GithubOrganization)
+  set(GIT_DOCUMENTATION_REPO ${GIT_ORIGIN_org})
+endif()
+
+if(NOT PROJECT_PACKAGE_NAME)
+  set(PROJECT_PACKAGE_NAME ${COMMON_PROJECT_DOMAIN}.${LOWER_PROJECT_NAME})
+  message(STATUS "Using ${PROJECT_PACKAGE_NAME} for documentation")
+endif()
+
+if(NOT COMMON_ORGANIZATION_NAME)
+  set(COMMON_ORGANIZATION_NAME Unknown)
+  message(STATUS "Using ${COMMON_ORGANIZATION_NAME} as origanization")
+endif()
+
+if(NOT DOXYGEN_CONFIG_FILE)
+  # Assuming there exists a Doxyfile and that needs configuring
+  configure_file(${CMAKE_CURRENT_LIST_DIR}/Doxyfile
+    ${CMAKE_BINARY_DIR}/doc/Doxyfile @ONLY)
+  set(DOXYGEN_CONFIG_FILE ${CMAKE_BINARY_DIR}/doc/Doxyfile)
 endif()
 
 add_custom_target(doxygen_install
@@ -42,10 +66,6 @@ install(DIRECTORY ${CMAKE_BINARY_DIR}/doc/html
   DESTINATION ${DOC_DIR}/API
   COMPONENT doc CONFIGURATIONS Release)
 
-if(NOT GIT_DOCUMENTATION_REPO)
-  include(GithubOrganization)
-  set(GIT_DOCUMENTATION_REPO ${GIT_ORIGIN_org})
-endif()
 if(GIT_DOCUMENTATION_REPO)
   set(GIT_DOCUMENTATION_DIR
     ${CMAKE_SOURCE_DIR}/../${GIT_DOCUMENTATION_REPO}/${PROJECT_NAME}-${VERSION_MAJOR}.${VERSION_MINOR})
