0293b492fcc651d8bfd66d6c04401a0e67bd864a
John Biddiscombe <biddisco@cscs.ch>
2014-08-10 19:32:48 +0200

    Add NO_UPDATE and VERBOSE options to GitExternal
    NO_UPDATE can be used to stop branch changes/updates.
    VERBOSE displays info about what is being done.

--- GitExternal.cmake
+++ GitExternal.cmake
@@ -7,8 +7,18 @@
 #    update target to bump the tag to the master revision by
 #    recreating .gitexternals.
 #  * Provides function
-#    git_external(<directory> <giturl> <gittag> [RESET <files>])
+#    git_external(<directory> <giturl> <gittag> [NO_UPDATE, VERBOSE] [RESET <files>])
 #  git_external_manage(<file>)
+#
+# [optional] Flags which control behaviour
+#  NO_UPDATE
+#    When set, GitExternal will not change a repo that has already been checked out. 
+#    The purpose of this is to allow one to set a default branch to be checked out, 
+#    but stop GitExternal from changing back to that branch if the user has checked 
+#    out and is working on another.
+#  VERBOSE 
+#    When set, displays information about git commands that are executed  
+#
 
 find_package(Git)
 if(NOT GIT_EXECUTABLE)
@@ -17,9 +27,16 @@ endif()
 
 include(CMakeParseArguments)
 
+macro(GIT_EXTERNAL_MESSAGE msg)
+  if(${GIT_EXTERNAL_VERBOSE})
+    message(STATUS "${NAME} : ${msg}")
+  endif()
+endmacro(GIT_EXTERNAL_MESSAGE)
+
 function(GIT_EXTERNAL DIR REPO TAG)
-  cmake_parse_arguments(GIT_EXTERNAL "" "" "RESET" ${ARGN})
-  get_filename_component(DIR "${DIR}" ABSOLUTE)
+  cmake_parse_arguments(GIT_EXTERNAL "NO_UPDATE;VERBOSE" "" "RESET" ${ARGN})
+  get_filename_component(DIR  "${DIR}" ABSOLUTE)
+  get_filename_component(NAME "${DIR}" NAME)
   get_filename_component(GIT_EXTERNAL_DIR "${DIR}/.." ABSOLUTE)
 
   if(NOT EXISTS "${DIR}")
@@ -34,51 +51,58 @@ function(GIT_EXTERNAL DIR REPO TAG)
   endif()
 
   if(IS_DIRECTORY "${DIR}/.git")
-    execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
-      OUTPUT_VARIABLE currentref OUTPUT_STRIP_TRAILING_WHITESPACE
-      WORKING_DIRECTORY ${DIR})
-    if(currentref STREQUAL TAG) # nothing to do
-      return()
-    endif()
+    if (${GIT_EXTERNAL_NO_UPDATE})
+      GIT_EXTERNAL_MESSAGE("Update branch disabled by user")
+    else()
+      GIT_EXTERNAL_MESSAGE("current ref is \"${currentref}\" and tag is \"${TAG}\"")
+      if(currentref STREQUAL TAG) # nothing to do
+        return()
+      endif()
 
-    # reset generated files
-    foreach(GIT_EXTERNAL_RESET_FILE ${GIT_EXTERNAL_RESET})
-      execute_process(
-        COMMAND "${GIT_EXECUTABLE}" reset -q "${GIT_EXTERNAL_RESET_FILE}"
+      # reset generated files
+      foreach(GIT_EXTERNAL_RESET_FILE ${GIT_EXTERNAL_RESET})
+        GIT_EXTERNAL_MESSAGE("git reset -q ${GIT_EXTERNAL_RESET_FILE}")
+        execute_process(
+          COMMAND "${GIT_EXECUTABLE}" reset -q "${GIT_EXTERNAL_RESET_FILE}"
+          RESULT_VARIABLE nok ERROR_VARIABLE error
+          WORKING_DIRECTORY "${DIR}")
+        GIT_EXTERNAL_MESSAGE("git checkout -q -- ${GIT_EXTERNAL_RESET_FILE}")
+        execute_process(
+          COMMAND "${GIT_EXECUTABLE}" checkout -q -- "${GIT_EXTERNAL_RESET_FILE}"
+          RESULT_VARIABLE nok ERROR_VARIABLE error
+          WORKING_DIRECTORY "${DIR}")
+      endforeach()
+
+      # fetch latest update
+      GIT_EXTERNAL_MESSAGE("git fetch --all -q")
+      execute_process(COMMAND "${GIT_EXECUTABLE}" fetch --all -q
         RESULT_VARIABLE nok ERROR_VARIABLE error
         WORKING_DIRECTORY "${DIR}")
+      if(nok)
+        message(STATUS "Update of ${DIR} failed:\n   ${error}")
+      endif()
+
+      # checkout requested tag
+      GIT_EXTERNAL_MESSAGE("git checkout -q ${TAG}")
       execute_process(
-        COMMAND "${GIT_EXECUTABLE}" checkout -q -- "${GIT_EXTERNAL_RESET_FILE}"
+        COMMAND "${GIT_EXECUTABLE}" checkout -q "${TAG}"
         RESULT_VARIABLE nok ERROR_VARIABLE error
+        WORKING_DIRECTORY "${DIR}"
+        )
+      if(nok)
+        message(STATUS "${DIR} git checkout ${TAG} failed: ${error}\n")
+      endif()
+
+      # update tag
+      GIT_EXTERNAL_MESSAGE("git rebase FETCH_HEAD")
+      execute_process(COMMAND ${GIT_EXECUTABLE} rebase FETCH_HEAD
+        RESULT_VARIABLE RESULT OUTPUT_VARIABLE OUTPUT ERROR_VARIABLE OUTPUT
         WORKING_DIRECTORY "${DIR}")
-    endforeach()
-
-    # fetch latest update
-    execute_process(COMMAND "${GIT_EXECUTABLE}" fetch --all -q
-      RESULT_VARIABLE nok ERROR_VARIABLE error
-      WORKING_DIRECTORY "${DIR}")
-    if(nok)
-      message(STATUS "Update of ${DIR} failed:\n   ${error}")
-    endif()
-
-    # checkout requested tag
-    execute_process(
-      COMMAND "${GIT_EXECUTABLE}" checkout -q "${TAG}"
-      RESULT_VARIABLE nok ERROR_VARIABLE error
-      WORKING_DIRECTORY "${DIR}"
-      )
-    if(nok)
-      message(STATUS "${DIR} git checkout ${TAG} failed: ${error}\n")
-    endif()
-
-    # update tag
-    execute_process(COMMAND ${GIT_EXECUTABLE} rebase FETCH_HEAD
-      RESULT_VARIABLE RESULT OUTPUT_VARIABLE OUTPUT ERROR_VARIABLE OUTPUT
-      WORKING_DIRECTORY "${DIR}")
-    if(RESULT)
-      message(STATUS "git rebase failed, aborting ${DIR} merge")
-      execute_process(COMMAND ${GIT_EXECUTABLE} rebase --abort
-        WORKING_DIRECTORY "${DIR}")
+      if(RESULT)
+        message(STATUS "git rebase failed, aborting ${DIR} merge")
+        execute_process(COMMAND ${GIT_EXECUTABLE} rebase --abort
+          WORKING_DIRECTORY "${DIR}")
+      endif()
     endif()
   else()
     message(STATUS "Can't update git external ${DIR}: Not a git repository")
