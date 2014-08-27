64c3ec1bbf324dbee8f2180c7c6d23034730726e
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-08-27 13:24:17 +0200

    Fix flatc_target

--- Findflatbuffers.cmake
+++ Findflatbuffers.cmake
@@ -21,12 +21,12 @@ if(FLATBUFFERS_FOUND)
     foreach(FILE ${ARGN})
       get_filename_component(FLATC_OUTPUT ${FILE} NAME_WE)
       set(FLATC_OUTPUT
-        "${PROJECT_BINARY_DIR}/${FLATC_OUTPUT}_generated.h")
+        "${CMAKE_CURRENT_BINARY_DIR}/${FLATC_OUTPUT}_generated.h")
       list(APPEND FLATC_OUTPUTS ${FLATC_OUTPUT})
 
       add_custom_command(OUTPUT ${FLATC_OUTPUT}
         COMMAND ${FLATC_EXECUTABLE}
-        ARGS -c -o "${PROJECT_BINARY_DIR}/" ${FILE}
+        ARGS -c -o "${CMAKE_CURRENT_BINARY_DIR}/" ${FILE}
         COMMENT "Building C++ header for ${FILE}"
         DEPENDS ${FILE}
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
@@ -35,7 +35,7 @@ if(FLATBUFFERS_FOUND)
   endfunction()
 
   set(FLATBUFFERS_INCLUDE_DIRS ${FLATBUFFERS_INCLUDE_DIR})
-  include_directories(${PROJECT_BINARY_DIR})
+  include_directories(${CMAKE_CURRENT_BINARY_DIR})
 else()
   set(FLATBUFFERS_INCLUDE_DIR)
 endif()
