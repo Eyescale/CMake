11535b48623d203196362544906310ce9b9f7b99
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-11-11 14:31:15 +0100

    CommonPackage: fix version with empty args

--- CommonPackage.cmake
+++ CommonPackage.cmake
@@ -1,10 +1,10 @@
 # Copyright (c) 2014 Stefan.Eilemann@epfl.ch
 
-# Provides common_package(Name args) which improves find_package First
-# invokes find_package with all the given arguments, and then falls
-# back to using pkg_config if available. The pkg_config path does only
-# implement the version, REQUIRED and QUIET find_package arguments
-# (e.g. no COMPONENTS)
+# Provides common_package(Name args) which improves find_package.
+# First invokes find_package with all the given arguments, and then
+# falls back to using pkg_config if available. The pkg_config path
+# does only implement the version, REQUIRED and QUIET find_package
+# arguments (e.g. no COMPONENTS)
 
 find_package(PkgConfig)
 set(ENV{PKG_CONFIG_PATH}
@@ -13,6 +13,7 @@ set(ENV{PKG_CONFIG_PATH}
 macro(COMMON_PACKAGE Name)
   string(TOUPPER ${Name} COMMON_PACKAGE_NAME)
   set(COMMON_PACKAGE_ARGS ${ARGN}) # ARGN is not a list. make one.
+  set(COMMON_PACKAGE_VERSION)
 
   if(COMMON_PACKAGE_ARGS)
     list(GET COMMON_PACKAGE_ARGS 0 COMMON_PACKAGE_VERSION)
@@ -27,7 +28,7 @@ macro(COMMON_PACKAGE Name)
   if(COMMON_PACKAGE_QUIET_POS EQUAL -1)
     set(COMMON_PACKAGE_QUIET)
   else()
-     set(COMMON_PACKAGE_QUIET "QUIET")
+    set(COMMON_PACKAGE_QUIET "QUIET")
   endif()
 
   list(FIND COMMON_PACKAGE_ARGS "REQUIRED" COMMON_PACKAGE_REQUIRED_POS)
