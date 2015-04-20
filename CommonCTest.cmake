2b5e50e21d97172a6d3dda999a20e9dee3d9d03a
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2015-04-20 16:30:55 +0200

    Fix test and cppcheck targets

--- CommonCTest.cmake
+++ CommonCTest.cmake
@@ -1,25 +1,21 @@
 # Copyright (c) 2010 Daniel Pfeifer
-#               2010-2014, Stefan Eilemann <eile@eyescale.ch>
+#               2010-2015, Stefan Eilemann <eile@eyescale.ch>
 #               2014, Juan Hernando <jhernando@fi.upm.es>
-
-include(CommonCPPCTest)
 
 if(NOT TARGET tests)
   add_custom_target(tests)
+endif()
+if(NOT TARGET perftests)
   add_custom_target(perftests)
-  set_target_properties(tests PROPERTIES FOLDER "Tests")
-  set_target_properties(perftests PROPERTIES FOLDER "Tests")
 endif()
 if(NOT TARGET ${PROJECT_NAME}_tests)
-  add_custom_target(${PROJECT_NAME}_tests DEPENDS ${PROJECT_NAME}_run_cpp_tests)
-  set_target_properties(${PROJECT_NAME}_tests PROPERTIES FOLDER "Tests")
-  add_dependencies(tests ${PROJECT_NAME}_tests)
-  add_custom_target(${PROJECT_NAME}_perftests
-    DEPENDS ${PROJECT_NAME}_run_perf_tests)
-  set_target_properties(${PROJECT_NAME}_perftests PROPERTIES FOLDER "Perftests")
-  add_dependencies(perftests ${PROJECT_NAME}_perftests)
+  add_custom_target(${PROJECT_NAME}_tests)
 endif()
 
-if(COVERAGE)
-  coverage_report(${PROJECT_NAME}_run_cpp_tests)
-endif()
+set_target_properties(tests PROPERTIES FOLDER "Tests")
+set_target_properties(perftests PROPERTIES FOLDER "Tests")
+set_target_properties(${PROJECT_NAME}_tests PROPERTIES FOLDER "Tests")
+
+add_dependencies(tests ${PROJECT_NAME}_tests)
+
+include(CommonCPPCTest)
