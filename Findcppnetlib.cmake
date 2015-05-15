ced7a0642e0ad3b457889a66880e2b52f3afe27a
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2015-05-15 16:46:09 +0200

    Make Findcppnetlib subproject-capable

--- Findcppnetlib.cmake
+++ Findcppnetlib.cmake
@@ -27,8 +27,8 @@
 #
 # =============================================================================
 #
-# CPPNETLIB 
-# 
+# CPPNETLIB
+#
 #==============================================================================
 # This module uses the following input variables:
 #
@@ -50,49 +50,54 @@ SET(CPPNETLIB_FOUND FALSE)
 # PATH ________________________________________________________________________
 
 if(NOT CPPNETLIB_PATH)
-    find_path(CPPNETLIB_PATH include/boost/network.hpp
-	HINTS ${CPPNETLIB_ROOT} $ENV{CPPNETLIB_ROOT}
-        /usr/local/
-        /usr/
-    )
-endif ()
+  find_path(CPPNETLIB_INCLUDE_DIR boost/network.hpp
+    HINTS ${CPPNETLIB_ROOT}/include $ENV{CPPNETLIB_ROOT}/include
+    ${CMAKE_SOURCE_DIR}/cppnetlib
+    /usr/local/include
+    /usr/include)
+  set(CPPNETLIB_PATH "${CPPNETLIB_INCLUDE_DIR}/..")
+endif()
 
 # HEADERS AND DYNAMIC LIBRARIES_________________________________________________
 
 if(CPPNETLIB_PATH)
-    set (CPPNETLIB_INCLUDE_DIR ${CPPNETLIB_PATH}/include)
-    mark_as_advanced (CPPNETLIB_INCLUDE_DIR)
+  mark_as_advanced (CPPNETLIB_INCLUDE_DIR)
+  set(__libraries
+    cppnetlib-client-connections cppnetlib-server-parsers cppnetlib-uri)
 
-    find_library(CPPNETLIB_LIBRARY1 NAMES cppnetlib-client-connections
-	HINTS ${CPPNETLIB_ROOT} $ENV{CPPNETLIB_ROOT}
-        PATHS ${CPPNETLIB_PATH}/lib64 ${CPPNETLIB_PATH}/lib
-    )
-    find_library(CPPNETLIB_LIBRARY2 NAMES cppnetlib-server-parsers
+  foreach(__library ${__libraries})
+    if(TARGET ${__library})
+      list(APPEND CPPNETLIB_LIBRARIES ${__library})
+    else()
+      find_library(${__library} NAMES ${__library}
         HINTS ${CPPNETLIB_ROOT} $ENV{CPPNETLIB_ROOT}
-        PATHS ${CPPNETLIB_PATH}/lib64 ${CPPNETLIB_PATH}/lib
-    )
-    find_library(CPPNETLIB_LIBRARY3 NAMES cppnetlib-uri
-        HINTS ${CPPNETLIB_ROOT} $ENV{CPPNETLIB_ROOT}
-        PATHS ${CPPNETLIB_PATH}/lib64 ${CPPNETLIB_PATH}/lib
-    )
-    SET(CPPNETLIB_LIBRARIES ${CPPNETLIB_LIBRARY1} ${CPPNETLIB_LIBRARY2} ${CPPNETLIB_LIBRARY3})
-    mark_as_advanced(CPPNETLIB_LIBRARIES)
+        PATHS ${CPPNETLIB_PATH}/lib64 ${CPPNETLIB_PATH}/lib)
+      list(APPEND CPPNETLIB_LIBRARIES ${${__library}})
+    endif()
+  endforeach()
+  mark_as_advanced(CPPNETLIB_LIBRARIES)
 endif()
 
 # FOUND _______________________________________________________________________
 if(CPPNETLIB_FIND_REQUIRED)
-    set(_cppnetlib_output 1)
+  set(_cppnetlib_output 1)
 else()
-    if(NOT CPPNETLIB_FIND_QUIETLY)
-        set(_cppnetlib_output 1)
-    endif()
+  if(NOT CPPNETLIB_FIND_QUIETLY)
+    set(_cppnetlib_output 1)
+  endif()
 endif()
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(CPPNETLIB DEFAULT_MSG CPPNETLIB_LIBRARIES CPPNETLIB_INCLUDE_DIR)
 
-set(CPPNETLIB_INCLUDE_DIRS ${CPPNETLIB_INCLUDE_DIR})
-
-if(CPPNETLIB_FOUND AND _cppnetlib_output )
-    message(STATUS "Found cpp-netlib in ${CPPNETLIB_INCLUDE_DIR};${CPPNETLIB_LIBRARIES}")
+if(CPPNETLIB_FOUND)
+  set(CPPNETLIB_INCLUDE_DIRS ${CPPNETLIB_INCLUDE_DIR})
+  if(_cppnetlib_output )
+    message(STATUS "Found cpp-netlib in ${CPPNETLIB_INCLUDE_DIR}:${CPPNETLIB_LIBRARIES}")
+  endif()
+else()
+  set(CPPNETLIB_FOUND)
+  set(CPPNETLIB_INCLUDE_DIR)
+  set(CPPNETLIB_INCLUDE_DIRS)
+  set(CPPNETLIB_LIBRARIES)
 endif()
