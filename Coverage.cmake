3b64f257f191a9decd18f0b9c9fb2d62383b6a84
Juan Hernando <jhernando@fi.upm.es>
2015-05-31 00:42:25 +0200

    Make add_coverage_targets a true function.
    So it can be called multiple times.

--- Coverage.cmake
+++ Coverage.cmake
@@ -65,35 +65,42 @@ function(add_coverage_targets TEST_TARGET)
     set(COVERAGE_LIMITS --rc genhtml_med_limit=40 --rc genhtml_hi_limit=80)
   endif()
 
-  add_custom_target(lcov-clean_${PROJECT_NAME}
-    COMMAND ${LCOV} -q --directory ${PROJECT_BINARY_DIR} --zerocounters
-    COMMENT "Resetting code coverage counters")
-
+  if(NOT TARGET lcov-clean_${PROJECT_NAME})
+    add_custom_target(lcov-clean_${PROJECT_NAME}
+      COMMAND ${LCOV} -q --directory ${PROJECT_BINARY_DIR} --zerocounters
+      COMMENT "Resetting code coverage counters")
+  endif()
   add_dependencies(${TEST_TARGET} lcov-clean_${PROJECT_NAME})
 
-  add_custom_target(lcov-gather_${PROJECT_NAME}
-    COMMAND ${LCOV} -q --capture --directory . --no-external
-      --directory ${PROJECT_SOURCE_DIR}/${PROJECT_INCLUDE_NAME}
-      --output-file lcov.info
-    COMMENT "Capturing code coverage counters"
-    WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-    DEPENDS ${TEST_TARGET})
+  if(NOT TARGET lcov-gather_${PROJECT_NAME})
+    add_custom_target(lcov-gather_${PROJECT_NAME}
+      COMMAND ${LCOV} -q --capture --directory . --no-external
+        --directory ${PROJECT_SOURCE_DIR}/${PROJECT_INCLUDE_NAME}
+        --output-file lcov.info
+      COMMENT "Capturing code coverage counters"
+      WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
+  endif()
+  add_dependencies(lcov-gather_${PROJECT_NAME} ${TEST_TARGET})
 
-  add_custom_target(lcov-remove_${PROJECT_NAME}
-    COMMAND ${LCOV} -q --remove lcov.info 'tests/*' '*.l' 'CMake/test/*'
-      '*/install/*' '${PROJECT_BINARY_DIR}/*' ${LCOV_EXCLUDE}
-      --output-file lcov2.info
-    COMMENT "Cleaning up code coverage counters"
-    WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-    DEPENDS lcov-gather_${PROJECT_NAME})
+  if(NOT TARGET lcov-remove_${PROJECT_NAME})
+    add_custom_target(lcov-remove_${PROJECT_NAME}
+      COMMAND ${LCOV} -q --remove lcov.info 'tests/*' '*.l' 'CMake/test/*'
+        '*/install/*' '${PROJECT_BINARY_DIR}/*' ${LCOV_EXCLUDE}
+        --output-file lcov2.info
+      COMMENT "Cleaning up code coverage counters"
+      WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+      DEPENDS lcov-gather_${PROJECT_NAME})
+  endif()
 
-  add_custom_target(coverage_${PROJECT_NAME}
-    COMMAND ${GENHTML} -q --title ${PROJECT_NAME} ${COVERAGE_LIMITS}
-      -o CoverageReport ${PROJECT_BINARY_DIR}/lcov2.info
-    COMMENT "Creating html coverage report, open ${PROJECT_BINARY_DIR}/doc/html/CoverageReport/index.html "
-    WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/doc/html
-    DEPENDS lcov-remove_${PROJECT_NAME})
-  make_directory(${PROJECT_BINARY_DIR}/doc/html)
+  if(NOT TARGET coverage_${PROJECT_NAME})
+    add_custom_target(coverage_${PROJECT_NAME}
+      COMMAND ${GENHTML} -q --title ${PROJECT_NAME} ${COVERAGE_LIMITS}
+        -o CoverageReport ${PROJECT_BINARY_DIR}/lcov2.info
+      COMMENT "Creating html coverage report, open ${PROJECT_BINARY_DIR}/doc/html/CoverageReport/index.html "
+      WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/doc/html
+      DEPENDS lcov-remove_${PROJECT_NAME})
+    make_directory(${PROJECT_BINARY_DIR}/doc/html)
+  endif()
 
   if(NOT TARGET coverage)
     add_custom_target(coverage)
