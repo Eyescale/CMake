Eyescale/CMake

commit e93d84385e7e1f55c00185fa8fc7af9b5944303e

More silence in CMake run and finders, remove unused BuildApplication

ChoosePython.cmake
@@ -38,7 +38,7 @@ set(USE_PYTHON_VERSION "auto" CACHE STRING
 
 set_property(CACHE USE_PYTHON_VERSION PROPERTY STRINGS 2 3 auto)
 
-if(NOT PYTHONINTERP_FOUND AND ${USE_PYTHON_VERSION} STREQUAL auto)
+if(NOT PYTHON_EXECUTABLE AND ${USE_PYTHON_VERSION} STREQUAL auto)
   # Finding Boost first if needed because if the Python3 interpreter is found
   # first there's no way back.
   if(CHOOSE_PYTHON_REQUIRE_BOOST)
@@ -48,7 +48,7 @@ if(NOT PYTHONINTERP_FOUND AND ${USE_PYTHON_VERSION} STREQUAL auto)
     find_package(PythonInterp 3 QUIET)
   endif()
 
-  if(PYTHONINTERP_FOUND AND (NOT CHOOSE_PYTHON_REQUIRE_BOOST OR Boost_FOUND))
+  if(PYTHON_EXECUTABLE AND (NOT CHOOSE_PYTHON_REQUIRE_BOOST OR Boost_FOUND))
     set(USE_PYTHON_VERSION 3)
     message(STATUS "Python 3 chosen automatically")
   else()
@@ -82,9 +82,10 @@ else()
   set(PythonInterp_FIND_VERSION 2)
 endif()
 
-find_package(PythonInterp) # Regardless of auto-detection, now we need to
-                           # find the interpreter to query the library suffix
+if(NOT PYTHON_EXECUTABLE)
+  find_package(PythonInterp QUIET) # Regardless of auto-detection, now we need to
+                                   # find the interpreter to query the library suffix
+endif()
 execute_process(COMMAND
   ${PYTHON_EXECUTABLE} -c "from distutils.sysconfig import get_python_lib; print(get_python_lib(1,0,prefix=''))"
   OUTPUT_VARIABLE PYTHON_LIBRARY_SUFFIX OUTPUT_STRIP_TRAILING_WHITESPACE)
-message(STATUS "Python package suffix path: ${PYTHON_LIBRARY_SUFFIX}")

BuildApplication.cmake
@@ -1,23 +1,0 @@
-# Copyright (c) 2014 Stefan.Eilemann@epfl.ch
-
-# Configures the build for a simple application:
-#   build_application(<Name>)
-#
-# Uses:
-# * NAME_SOURCES for all compilation units
-# * NAME_HEADERS for all internal header files
-# * NAME_LINK_LIBRARIES for dependencies of name
-#
-# Builds Name application and installs it.
-
-function(BUILD_APPLICATION Name)
-  string(TOUPPER ${Name} NAME)
-  string(TOLOWER ${Name} name)
-  set(SOURCES ${${NAME}_SOURCES})
-  set(HEADERS ${${NAME}_HEADERS})
-  set(LINK_LIBRARIES ${${NAME}_LINK_LIBRARIES})
-
-  add_executable(${Name} ${HEADERS} ${SOURCES})
-  target_link_libraries(${Name} ${LINK_LIBRARIES})
-  install(TARGETS ${Name} DESTINATION bin COMPONENT apps)
-endfunction()

DoxygenRule.cmake
@@ -44,7 +44,9 @@
 #   When using subprojects, it is the responsiblity of the user to clone
 #   the documentation repository in the project's parent folder.
 
-find_package(Doxygen QUIET)
+if(NOT DOXYGEN_FOUND)
+  find_package(Doxygen QUIET)
+endif()
 if(NOT DOXYGEN_FOUND)
   return()
 endif()
@@ -65,7 +67,6 @@ if(NOT PROJECT_PACKAGE_NAME)
     message(STATUS "Set COMMON_PROJECT_DOMAIN to ${COMMON_PROJECT_DOMAIN}")
   endif()
   set(PROJECT_PACKAGE_NAME ${COMMON_PROJECT_DOMAIN}.${LOWER_PROJECT_NAME})
-  message(STATUS "Using ${PROJECT_PACKAGE_NAME} for documentation")
 endif()
 
 if(NOT DOXYGEN_PROJECT_NAME)
@@ -74,7 +75,6 @@ endif()
 
 if(NOT COMMON_ORGANIZATION_NAME)
   set(COMMON_ORGANIZATION_NAME Unknown)
-  message(STATUS "Using ${COMMON_ORGANIZATION_NAME} as organization")
 endif()
 
 if(NOT DOXYGEN_CONFIG_FILE)

FindNumPy.cmake
@@ -5,11 +5,13 @@
 
 cmake_minimum_required(VERSION 2.6)
 
-if(NUMPY_FIND_QUIETLY)
-  find_package(PythonInterp QUIET)
-else()
-  find_package(PythonInterp)
-  set(_numpy_out 1)
+if(NOT PYTHON_EXECUTABLE)
+  if(NumPy_FIND_QUIETLY)
+    find_package(PythonInterp QUIET)
+  else()
+    find_package(PythonInterp)
+    set(_numpy_out 1)
+  endif()
 endif()
 
 if (PYTHON_EXECUTABLE)

FindTuvok.cmake
@@ -12,16 +12,11 @@
 #  TUVOK_LIBRARY - The libraries needed to use SAGE
 include(FindPackageHandleStandardArgs)
 
-if(TUVOK_FIND_REQUIRED)
+if(Tuvok_FIND_REQUIRED)
   set(_TUVOK_required REQUIRED)
-  set(_TUVOK_output 1)
-else()
-  if(NOT TUVOK_FIND_QUIETLY)
-    set(_TUVOK_output 1)
-  endif()
 endif()
-if(TUVOK_FIND_QUIETLY)
-  set(_TUVOK_quiet QUIET)
+if(NOT Tuvok_FIND_QUIETLY)
+  set(_TUVOK_output 1)
 endif()
 
 find_path(_TUVOK_INCLUDE_DIR NAMES Tuvok/StdTuvokDefines.h
@@ -43,14 +38,14 @@ find_library(TUVOK_LIBRARY Tuvok
   PATH_SUFFIXES lib lib64
   PATHS /usr/local /usr /usr/local /opt /opt/local)
 
-if(TUVOK_FIND_REQUIRED)
+if(Tuvok_FIND_REQUIRED)
  if(TUVOK_LIBRARY MATCHES "TUVOK_LIBRARY-NOTFOUND")
    message(FATAL_ERROR "Missing the Tuvok library.\n"
      "Consider using CMAKE_PREFIX_PATH or the TUVOK_ROOT environment variable. "
      "See the ${CMAKE_CURRENT_LIST_FILE} for more details.")
  endif()
 endif()
-find_package_handle_standard_args(TUVOK DEFAULT_MSG TUVOK_LIBRARY _TUVOK_INCLUDE_DIR)
+find_package_handle_standard_args(Tuvok DEFAULT_MSG TUVOK_LIBRARY _TUVOK_INCLUDE_DIR)
 
 if(_TUVOK_FAIL)
   # Zero out everything, we didn't meet version requirements

FindVTune.cmake
@@ -8,13 +8,6 @@ if(CMAKE_VERSION VERSION_LESS 2.8.3) # WAR bug
   get_filename_component(CMAKE_CURRENT_LIST_DIR ${CMAKE_CURRENT_LIST_FILE} PATH)
 endif()
 
-if(VTune_FIND_REQUIRED)
-  set(_vtune_output 1)
-  set(VTUNE_FIND_REQUIRED 1)
-elseif(NOT VTune_FIND_QUIETLY)
-  set(_vtune_output 1)
-endif()
-
 find_program(VTUNE_EXECUTABLE amplxe-cl)
 if(NOT VTUNE_EXECUTABLE)
   set(VTUNE_FOUND)
@@ -36,7 +29,7 @@ find_library(VTUNE_ITTNOTIFY_LIBRARY NAMES ittnotify
   PATHS /usr/lib /usr/local/lib /opt/local/lib)
 
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(VTUNE DEFAULT_MSG
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(VTune DEFAULT_MSG
   VTUNE_INCLUDE_DIR VTUNE_ITTNOTIFY_LIBRARY)
 
 set(VTUNE_INCLUDE_DIRS ${VTUNE_INCLUDE_DIR})
@@ -47,7 +40,3 @@ endif()
 set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -DINTEL_NO_ITTNOTIFY_API")
 set(CMAKE_CXX_FLAGS_RELEASE
   "${CMAKE_CXX_FLAGS_RELEASE} -DINTEL_NO_ITTNOTIFY_API")
-
-if(_vtune_output AND VTUNE_FOUND)
-  message(STATUS "Found VTune in ${VTUNE_INCLUDE_DIRS};${VTUNE_LIBRARIES}")
-endif()

Findcpplint.cmake
@@ -18,24 +18,24 @@
 #  FindPythonLibs
 
 # Check python installation as cpplint is a python script
-if (NOT PYTHONINTERP_FOUND)
+if(NOT PYTHON_EXECUTABLE)
   find_package(PythonInterp QUIET)
-endif(NOT PYTHONINTERP_FOUND)
+endif()
 
-if(NOT PYTHONLIBS_FOUND)
+if(NOT PYTHON_FOUND)
  find_package(PythonLibs QUIET)
-endif(NOT PYTHONLIBS_FOUND)
+endif()
 
-if (PYTHONINTERP_FOUND AND PYTHONLIBS_FOUND)
+if(PYTHON_EXECUTABLE AND PYTHON_FOUND)
   set(_python_found true)
-endif(PYTHONINTERP_FOUND AND PYTHONLIBS_FOUND)
+endif()
 
 file(TO_CMAKE_PATH "${CPPLINT_ROOT_DIR}" CPPLINT_ROOT_DIR)
 set(CPPLINT_ROOT_DIR
-	"${CPPLINT_ROOT_DIR}"
-	CACHE
-	PATH
-	"Path to search for cpplint")
+    "${CPPLINT_ROOT_DIR}"
+    CACHE
+    PATH
+    "Path to search for cpplint")
 
 if(CPPLINT_SCRIPT AND NOT EXISTS "${CPPLINT_SCRIPT}")
   set(CPPLINT_SCRIPT NOTFOUND CACHE PATH "" FORCE)
@@ -44,9 +44,9 @@ endif(CPPLINT_SCRIPT AND NOT EXISTS "${CPPLINT_SCRIPT}")
 # If we have a custom path, look there first.
 if(CPPLINT_ROOT_DIR)
   find_file(CPPLINT_SCRIPT
-	NAMES cpplint.py
-	PATHS "${CPPLINT_ROOT_DIR}"
-	NO_DEFAULT_PATH)
+    NAMES cpplint.py
+    PATHS "${CPPLINT_ROOT_DIR}"
+    NO_DEFAULT_PATH)
 endif(CPPLINT_ROOT_DIR)
 
 set(CPPLINT_SCRIPT_DEFAULT_LOCATIONS
@@ -70,7 +70,7 @@ else(_python_found)
   set(CPPLINT_NOT_FOUND_MSG "Could NOT find python needed to run cpplint.py. Please check both executable and libraries are installed.")
 endif(_python_found)
 
-find_package_handle_standard_args(cpplint "${CPPLINT_NOT_FOUND_MSG}" PYTHONINTERP_FOUND PYTHONLIBS_FOUND CPPLINT_SCRIPT)
+find_package_handle_standard_args(cpplint "${CPPLINT_NOT_FOUND_MSG}" PYTHON_EXECUTABLE PYTHON_FOUND CPPLINT_SCRIPT)
 
 if(CPPLINT_FOUND OR CPPLINT_MARK_AS_ADVANCED)
   mark_as_advanced(CPPLINT_ROOT_DIR)

Findcppnetlib.cmake
@@ -61,8 +61,8 @@ endif()
 
 # Find dynamic libraries
 if(CPPNETLIB_PATH)
-    set(__libraries cppnetlib-client-connections 
-                    cppnetlib-server-parsers 
+    set(__libraries cppnetlib-client-connections
+                    cppnetlib-server-parsers
                     cppnetlib-uri)
 
     foreach(__library ${__libraries})
@@ -78,23 +78,20 @@ if(CPPNETLIB_PATH)
     mark_as_advanced(CPPNETLIB_LIBRARIES)
 endif()
 
-if(CPPNETLIB_FIND_REQUIRED)
+
+if(NOT cppnetlib_FIND_QUIETLY)
   set(_cppnetlib_output 1)
-else()
-  if(NOT CPPNETLIB_FIND_QUIETLY)
-    set(_cppnetlib_output 1)
-  endif()
 endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(CPPNETLIB DEFAULT_MSG 
+find_package_handle_standard_args(cppnetlib DEFAULT_MSG
                                   CPPNETLIB_LIBRARIES
                                   CPPNETLIB_INCLUDE_DIR)
 
 if(CPPNETLIB_FOUND)
   set(CPPNETLIB_INCLUDE_DIRS ${CPPNETLIB_INCLUDE_DIR})
   if(_cppnetlib_output )
-    message(STATUS "Found cpp-netlib in ${CPPNETLIB_INCLUDE_DIR}:${CPPNETLIB_LIBRARIES}")
+    message(STATUS "Found cppnetlib in ${CPPNETLIB_INCLUDE_DIR}:${CPPNETLIB_LIBRARIES}")
   endif()
 else()
   set(CPPNETLIB_FOUND)

commit dc80b85337261b5bee50fd799f04482ffd732744

Fixing Python autodetection and adding a sanity check.
The script fails when Boost.Python3 is detected but Python 3 is not really
available.
The error is a regression from e93d8438.
PYTHON_EXECUTABLE mustn't be used because the PythonInterp script sets the
variable to any interpreter found even it has failed because the version
doesn't match the requested one.

ChoosePython.cmake
@@ -38,6 +38,11 @@ set(USE_PYTHON_VERSION "auto" CACHE STRING
 
 set_property(CACHE USE_PYTHON_VERSION PROPERTY STRINGS 2 3 auto)
 
+if(PYTHONINTERP_FOUND)
+  message(WARNING "This script must be included before trying to find Python.")
+  return()
+endif()
+
 if(${USE_PYTHON_VERSION} STREQUAL auto)
   # Finding Boost first if needed because if the Python3 interpreter is found
   # first there's no way back.
@@ -48,7 +53,7 @@ if(${USE_PYTHON_VERSION} STREQUAL auto)
     find_package(PythonInterp 3 QUIET)
   endif()
 
-  if(PYTHON_EXECUTABLE AND (NOT CHOOSE_PYTHON_REQUIRE_BOOST OR Boost_FOUND))
+  if(PYTHONINTERP_FOUND AND (NOT CHOOSE_PYTHON_REQUIRE_BOOST OR Boost_FOUND))
     set(USE_PYTHON_VERSION 3)
     message(STATUS "Python 3 chosen automatically")
   else()
@@ -83,8 +88,9 @@ else()
 endif()
 
 if(NOT PYTHON_EXECUTABLE)
-  find_package(PythonInterp QUIET) # Regardless of auto-detection, now we need to
-                                   # find the interpreter to query the library suffix
+  # Regardless of auto-detection, now we need to find the interpreter to
+  # query the library suffix.
+  find_package(PythonInterp QUIET)
 endif()
 execute_process(COMMAND
   ${PYTHON_EXECUTABLE} -c "from distutils.sysconfig import get_python_lib; print(get_python_lib(1,0,prefix=''))"