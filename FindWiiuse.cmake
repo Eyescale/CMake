5abea8063168a548038968bdc438779180043e67
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2015-07-23 16:51:49 +0200

    Tweak FindWiiuse.cmake

--- FindWiiuse.cmake
+++ FindWiiuse.cmake
@@ -3,52 +3,30 @@
 # WIIUSE 
 # _____________________________________________________________________________
 
-# Assume not found.
-SET(WIIUSE_FOUND FALSE)
-
 # PATH ________________________________________________________________________
 
-if (WIIUSE_PATH)
-    # Set by user...
-else (WIIUSE_PATH)
-    find_path(WIIUSE_PATH include/wiiuse.h
-	HINTS ${WIIUSE_ROOT} $ENV{WIIUSE_ROOT}
-        /usr/local/
-        /usr/
-    )
-endif (WIIUSE_PATH)
-
-if (WIIUSE_PATH)
-    set (WIIUSE_FOUND TRUE)
-endif (WIIUSE_PATH)
+if(NOT WIIUSE_PATH)
+  find_path(WIIUSE_PATH include/wiiuse.h
+            HINTS ${WIIUSE_ROOT} $ENV{WIIUSE_ROOT}
+            /usr/local/
+            /usr/)
+endif()
 
 # HEADERS _____________________________________________________________________
 
-if (WIIUSE_FOUND)
-    set (WIIUSE_INCLUDE_DIR ${WIIUSE_PATH}/include)
-    mark_as_advanced (WIIUSE_INCLUDE_DIR)
-endif (WIIUSE_FOUND)
+if(WIIUSE_PATH)
+  set (WIIUSE_INCLUDE_DIR ${WIIUSE_PATH}/include)
+  mark_as_advanced (WIIUSE_INCLUDE_DIR)
+endif()
 
 # STATIC LIBRARY ______________________________________________________________
 
-if (WIIUSE_FOUND)
-    find_library(WIIUSE_LIBRARIES NAMES wiiuse
-	HINTS ${WIIUSE_ROOT} $ENV{WIIUSE_ROOT}
-        PATHS ${WIIUSE_PATH}/lib
-    )
-    mark_as_advanced(WIIUSE_LIBRARIES)
-endif (WIIUSE_FOUND)
-
-# FOUND _______________________________________________________________________
-if(WIIUSE_FOUND)
-    message(STATUS
-      "Found WiiUse in ${WIIUSE_INCLUDE_DIR};${WIIUSE_LIBRARIES}")
+if(WIIUSE_PATH)
+  find_library(WIIUSE_LIBRARIES NAMES wiiuse
+               HINTS ${WIIUSE_ROOT} $ENV{WIIUSE_ROOT}
+               PATHS ${WIIUSE_PATH}/lib)
+  mark_as_advanced(WIIUSE_LIBRARIES)
 endif()
 
-if (NOT WIIUSE_FOUND)
-   if (WIIUSE_FIND_REQUIRED)
-      message(FATAL_ERROR "Could not find Wiiuse")
-   endif (WIIUSE_FIND_REQUIRED)
-endif (NOT WIIUSE_FOUND)
-  
-
+find_package_handle_standard_args(Wiiuse DEFAULT_MSG
+                                  WIIUSE_LIBRARIES WIIUSE_INCLUDE_DIR)
