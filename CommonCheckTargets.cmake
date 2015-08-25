ce563a802648e4a31d1f74f007001479bd29c24d
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2015-08-25 15:32:55 +0200

    Fix #410: Restore API of check targets

--- CommonCheckTargets.cmake
+++ CommonCheckTargets.cmake
@@ -3,6 +3,7 @@
 # Provide the function common_check_targets to add check targets (clangcheck,
 # cppcheck, cpplint) to the given target.
 
+include(GetSourceFilesFromTarget)
 include(clangcheckTargets)
 include(CppcheckTargets)
 include(CpplintTargets)
@@ -11,21 +12,12 @@ function(common_check_targets _name)
   set(_exclude_pattern ".*moc_|.*qrc_.*\\.c.*$") # Qt moc and qrc files
 
   # Get the list of files once for all check targets
-  get_target_property(_sources "${_name}" SOURCES)
-  set(_files)
-  foreach(_source ${_sources})
-    get_source_file_property(_lang "${_source}" LANGUAGE)
-    get_source_file_property(_loc "${_source}" LOCATION)
-    if("${_lang}" MATCHES "CXX" AND NOT ${_loc} MATCHES ${_exclude_pattern})
-      list(APPEND _files "${_loc}")
-    endif()
-  endforeach()
-
-  if(NOT _files)
+  get_source_files(${_name} ${_exclude_pattern})
+  if(NOT ${_name}_FILES)
     return()
   endif()
 
-  add_clangcheck(${_name} ${_files})
-  add_cppcheck(${_name} ${_files} POSSIBLE_ERROR FAIL_ON_WARNINGS)
-  add_cpplint(${_name} ${_files} CATEGORY_FILTER_OUT readability/streams)
+  add_clangcheck(${_name} FILES ${${_name}_FILES})
+  add_cppcheck(${_name} FILES ${${_name}_FILES} POSSIBLE_ERROR FAIL_ON_WARNINGS)
+  add_cpplint(${_name} FILES ${${_name}_FILES} CATEGORY_FILTER_OUT readability/streams)
 endfunction()
