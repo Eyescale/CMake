638394d1b23f8d2f8257abed6c8c4638c9ae624d
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2015-10-15 10:57:04 +0200

    CR #421

--- CommonDate.cmake
+++ CommonDate.cmake
@@ -2,11 +2,9 @@
 if(NOT COMMON_DATE)
   if(WIN32)
     execute_process(COMMAND "cmd" " /C date /T" OUTPUT_VARIABLE COMMON_DATE)
-    string(REGEX REPLACE "(..)/(..)/..(..).*" "\\1/\\2/\\3"
-      COMMON_DATE ${COMMON_DATE})
   else()
     execute_process(COMMAND "date" "+%d/%m/%Y" OUTPUT_VARIABLE COMMON_DATE)
-    string(REGEX REPLACE "(..)/(..)/..(..).*" "\\1/\\2/\\3"
-      COMMON_DATE ${COMMON_DATE})
   endif()
+  string(REGEX REPLACE "(..)/(..)/..(..).*" "\\1/\\2/\\3"
+    COMMON_DATE ${COMMON_DATE})
 endif()
