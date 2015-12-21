Eyescale/CMake

commit 1f21ab0679e2e0776b830e2093836efd9d19fb4b

Replace + by P in package names for define.h generation

CommonPackage.cmake
@@ -132,6 +132,7 @@ macro(common_package Package_Name)
     # for defines.h
     set(__use_package_define "${UPPER_PROJECT_NAME}_USE_${PACKAGE_NAME}")
     string(REGEX REPLACE "-" "_" __use_package_define ${__use_package_define})
+    string(REGEX REPLACE "+" "P" __use_package_define ${__use_package_define})
     list(APPEND COMMON_PACKAGE_DEFINES ${__use_package_define})
 
     # for CommonPackageConfig.cmake

commit b642f9e6dfc693fb48665f359ab223c0175586c8

Fix a regression in 1f21ab0.
Do no use regex replace for package name to define translation. Using
+ in a regex without escaping is wrong.

CommonPackage.cmake
@@ -131,8 +131,8 @@ macro(common_package Package_Name)
 
     # for defines.h
     set(__use_package_define "${UPPER_PROJECT_NAME}_USE_${PACKAGE_NAME}")
-    string(REGEX REPLACE "-" "_" __use_package_define ${__use_package_define})
-    string(REGEX REPLACE "+" "P" __use_package_define ${__use_package_define})
+    string(REPLACE "-" "_" __use_package_define ${__use_package_define})
+    string(REPLACE "+" "P" __use_package_define ${__use_package_define})
     list(APPEND COMMON_PACKAGE_DEFINES ${__use_package_define})
 
     # for CommonPackageConfig.cmake