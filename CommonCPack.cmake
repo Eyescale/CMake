Eyescale/CMake

commit a4f0acd5a026ea46ca54f656ffd193f509eab199

Fix for Ubuntu bionic lsb_codename

CommonCPack.cmake
@@ -202,7 +202,11 @@ if(CPACK_GENERATOR STREQUAL "DEB")
   # Note: the ~codename is not part of any standard and could be omitted.
   if(NOT CPACK_DEBIAN_PACKAGE_VERSION)
     include(LSBInfo)
-    set(CPACK_DEBIAN_PACKAGE_VERSION "${CPACK_PACKAGE_VERSION}~${LSB_CODENAME}")
+    if (${LSB_CODENAME} STREQUAL "bionic")
+      set(CPACK_DEBIAN_PACKAGE_VERSION "${CPACK_PACKAGE_VERSION}")
+    elseif()
+      set(CPACK_DEBIAN_PACKAGE_VERSION "${CPACK_PACKAGE_VERSION}~${LSB_CODENAME}")
+    endif()
   endif()
   if(NOT CPACK_PACKAGE_FILE_NAME)
     get_debian_arch(_deb_arch)

commit f35a3ed209d682f048e867481e4c8472a373ab5c

fixing bug introduced in  a4f0acd

CommonCPack.cmake
@@ -204,7 +204,7 @@ if(CPACK_GENERATOR STREQUAL "DEB")
     include(LSBInfo)
     if (${LSB_CODENAME} STREQUAL "bionic")
       set(CPACK_DEBIAN_PACKAGE_VERSION "${CPACK_PACKAGE_VERSION}")
-    elseif()
+    else()
       set(CPACK_DEBIAN_PACKAGE_VERSION "${CPACK_PACKAGE_VERSION}~${LSB_CODENAME}")
     endif()
   endif()